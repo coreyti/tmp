6ca17b85741c87b3bd0d0a31db2fc8ce152d1c38
Corey Innis <cinnis@pivotal.io>
2017-03-03 08:41:16 -0800

    Start SSH agent and fix director context

--- tasks/promote.sh
+++ tasks/promote.sh
@@ -3,23 +3,26 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 NOW=$(date)
 
+eval $(ssh-agent -s)
 ssh-add ${DIR}/secrets/id_rsa
 
-found=$(git tag --list | grep ${tag})
-timestamp=$(date '+%s')
+pushd repo > /dev/null
+  found=$(git tag --list | grep ${tag})
+  timestamp=$(date '+%s')
 
-git config --global user.email "coreyti+ci@gmail.com"
-git config --global user.name "Concourse Build"
+  git config --global user.email "coreyti+ci@gmail.com"
+  git config --global user.name "Concourse Build"
 
-if [ -n "${found}" ] ; then
-  git push origin :refs/tags/${tag}
+  if [ -n "${found}" ] ; then
+    git push origin :refs/tags/${tag}
 
-  echo "Changes as of ${NOW}"                     >> ${DIR}/CHANGELOG.txt
-  echo "----------------------------------------" >> ${DIR}/CHANGELOG.txt
-  git log ${tag}...HEAD                           >> ${DIR}/CHANGELOG.txt
-fi
+    echo "Changes as of ${NOW}"                     >> ${DIR}/CHANGELOG.txt
+    echo "----------------------------------------" >> ${DIR}/CHANGELOG.txt
+    git log ${tag}...HEAD                           >> ${DIR}/CHANGELOG.txt
+  fi
 
-git tag -fa "${tag}"
-git tag -fa "${tag}-${timestamp}"
-git commit -m "CI Promote at ${timestamp}"
-git push origin master --tags
+  git tag -fa "${tag}"
+  git tag -fa "${tag}-${timestamp}"
+  git commit -m "CI Promote at ${timestamp}"
+  git push origin master --tags
+popd > /dev/null
