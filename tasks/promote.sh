7314cdbbe62492b0319e268df6204fe9209ddfc0
Corey Innis <cinnis@pivotal.io>
2017-03-03 10:44:59 -0800

    Fix various issues in promote task
    - use better bash flags
    - fix `REPO_DIR`
    - don't fail if `.ssh` exists
    - fix/improve tag handling
    - improve `CHANGELOG` edit
    - add missing `git add` and `git commit`

--- tasks/promote.sh
+++ tasks/promote.sh
@@ -1,34 +1,44 @@
 #!/usr/bin/env bash
+set -euo pipefail
 
-NOW=$(date)
+apt-get -y update
+apt-get -y install vim-tiny
+EDITOR=vim.tiny
+
 BASE_DIR=$(pwd)
-REPO_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+REPO_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 
 eval $(ssh-agent -s)
 chmod 400 ./secrets/id_rsa
 ssh-add ./secrets/id_rsa
-mkdir ~/.ssh/
+mkdir -p ~/.ssh/
 cat "${BASE_DIR}/secrets/known_hosts" >> ~/.ssh/known_hosts
 
 pushd repo > /dev/null
   git fetch --tags
 
-  found=$(git tag --list | grep ${tag})
+  found=$(git tag --list ${tag})
+  datestamp=$(date)
   timestamp=$(date '+%s')
 
   git config --global user.email "coreyti+ci@gmail.com"
   git config --global user.name "Concourse Build"
 
+  echo "Changes as of ${datestamp}"               >> ${REPO_DIR}/CHANGELOG.txt
+  echo "----------------------------------------" >> ${REPO_DIR}/CHANGELOG.txt
+
   if [ -n "${found}" ] ; then
     git push origin :refs/tags/${tag}
-
-    echo "Changes as of ${NOW}"                     >> ${REPO_DIR}/CHANGELOG.txt
-    echo "----------------------------------------" >> ${REPO_DIR}/CHANGELOG.txt
-    git log ${tag}...HEAD                           >> ${REPO_DIR}/CHANGELOG.txt
+    git log ${tag}...HEAD >> ${REPO_DIR}/CHANGELOG.txt
+  else
+    git log $(git rev-list --max-parents=0 HEAD)...HEAD \
+      >> ${REPO_DIR}/CHANGELOG.txt
   fi
 
-  git tag -fa "${tag}"
-  git tag -fa "${tag}-${timestamp}"
-  git commit -m "CI Promote at ${timestamp}"
+
+  git add .
+  git tag -f "${tag}"
+  git tag -f "${tag}-${timestamp}"
+  git commit -m "CI Promote at ${datestamp}"
   git push origin master --tags
 popd > /dev/null
