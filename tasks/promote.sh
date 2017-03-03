36ecf997cf587c014b49842444c2acef920e6275
pivotal <pivotal@cinnis.local>
2017-03-02 23:14:54 -0800

    Add git author

--- tasks/promote.sh
+++ tasks/promote.sh
@@ -3,6 +3,9 @@
 pushd repo/tasks > /dev/null
   found=$(git tag --list | grep ${tag})
 
+  git config --global user.email "you@example.com"
+  git config --global user.name "Your Name"
+
   if [ -n "${found}" ] ; then
     git diff ${tag} > changelog.txt
     git push origin :refs/tags/${tag}
